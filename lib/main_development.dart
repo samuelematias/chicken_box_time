268137ba2fd90c6721ccbcfd8ac595d6db8df22d
Samuel Matias <hi@samuelematias.com>
2022-04-15 12:57:19 -0300

    feat: implement some improves on bootstrap the app

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,9 +1,10 @@
 import 'package:chicken_box_time/app/app.dart';
+import 'package:chicken_box_time/bootstrap.dart';
 import 'package:chicken_box_time/flavor/flavor.dart';
 import 'package:flutter/widgets.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   Flavor.setEnvironment(Environment.development);
-  runApp(const App());
+  bootstrap(() => const App());
 }
