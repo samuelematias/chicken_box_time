088ea785ed3e8a30c91d1d5382ddcec460d3c58c
Samuel Matias <hi@samuelematias.com>
2022-04-19 07:21:53 -0300

    feat: add repository on main files

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -2,11 +2,17 @@ import 'package:chicken_box_time/app/app.dart';
 import 'package:chicken_box_time/bootstrap.dart';
 import 'package:chicken_box_time/flavor/flavor.dart';
 import 'package:flutter/widgets.dart';
+import 'package:movie_repository/movie_repository.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   Flavor.setEnvironment(Environment.production);
+
+  final _movieRepository = MovieRepository();
+
   bootstrap(() {
-    return App();
+    return App(
+      movieRepository: _movieRepository,
+    );
   });
 }
